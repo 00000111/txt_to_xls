5e3e354fd81221f9b2f4312ab4f9fdb2b7ba4f59
Ivan Konstantinov <samlaughter@gmail.com>
2014-07-25 18:36:21 +0300

    DB
    Инит базы данных и запись инфы о логине !!! НЕ ОТЛАЖЕНО

--- txt_to_xls.py
+++ txt_to_xls.py
@@ -6,6 +6,39 @@
 
 import sys
 import xlwt
+import sqlite3
+
+# Создаем класс базы данных
+class Database(object):
+	def __init__(self, db_name = 'main.db', login_table_name = 'logins', logout_table_name = 'logouts'):
+		self.db_name = db_name
+		self.logins = login_table_name
+		self.logouts = logout_table_name
+	
+	# Метод для создающий таблицы для событий входа и выхода
+	def CreateDB(self):
+		conn = sqlite3.connect(self.db_name)
+		cur = conn.cursor()
+
+		cur.execute('''CREATE TABLE ? 
+					(LI_Id INTEGER NOT NULL UNIQUE, event text, li_date text, li_time text, username text)''', self.logins)
+		cur.execute('''CREATE TABLE ?
+					(LO_Id INTEGER NOT NULL UNIQUE, event text, lo_date text, lo_lime text, username text)''', self.logouts)
+		
+		conn.commit()
+		conn.close()
+
+	#Метод для записи ниформации о входе
+	def WriteLoginInfo(self, li_date, li_time, username):
+		conn = sqlite3.connect(self.db_name)
+		cur = conn.cursor()
+		
+		cur.execute("SELECT Li_Id FROM ? ORDER BY Li_Id DESC LIMIT 1", self.logins)
+		line_id = cur.fetcone()
+		line_id = line_id[0] + 1
+
+		cur.execute("""INSERT INTO ? (LI_Id, event, li_date, li_time, username) 
+					VALUES (?,?,?,?,?)""", self.logins (line_id, u'Вход', li_date, li_time, username))
 
 args = sys.argv
 source = "ACD-SRV.log"
